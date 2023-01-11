b4e847cc1c172e759f976ec4070aa05c2f0df0f1
fernando-yaeda <fernandoyudi.yaeda@gmail.com>
2023-01-11 13:28:17 -0300

    build(app): create init and close database

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import "express-async-errors";
 import express, { Express } from "express";
 import cors from "cors";
 
-import { loadEnv } from "./config";
+import { loadEnv, connectDb, disconnectDb } from "./config";
 
 loadEnv();
 
 const app = express();
 
 app.use(cors()).use(express.json());
+
+export function init(): Promise<Express> {
+  connectDb();
+  return Promise.resolve(app);
+}
+
+export async function close(): Promise<void> {
+  await disconnectDb();
+}
 
 export default app;
