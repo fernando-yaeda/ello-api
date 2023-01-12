238e9e734c7aa51ddd258dc91f5f9154925f212c
fernando-yaeda <fernandoyudi.yaeda@gmail.com>
2023-01-12 20:23:23 -0300

    feat(users): create user services
    create user services, duplicate email error, and create user schema

---
+++ src/schemas/user-schema.ts
@@ -0,0 +1,8 @@
+import { CreateUserParams } from "@/services/user-services";
+import Joi from "joi";
+
+export const createUserSchema = Joi.object<CreateUserParams>({
+  email: Joi.string().email().required(),
+  fullName: Joi.string().required(),
+  password: Joi.string().min(6).max(18).required(),
+});
