e82c5d5dc0c4ace1bedefca3dda9e233fc7b13ad
fernando-yaeda <fernandoyudi.yaeda@gmail.com>
2023-01-20 00:33:59 -0300

    fix(prisma): add sessions table

---
+++ prisma/migrations/20230120001051_create_sessions_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "sessions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
