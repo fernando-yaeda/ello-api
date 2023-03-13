d05658dd665fa8531aa57ffb5f4cea850fdf9c70
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-03-13 17:15:57 -0300

    refactor(app): add health route

--- src/errors/unauthorized-error.ts
+++ src/errors/unauthorized-error.ts
@@ -1,7 +1,6 @@
 import { ApplicationError } from "@/protocols";
 
 export function unauthorizedError(): ApplicationError {
-  console.log("testing unauthorized error");
   return {
     name: "UnauthorizedError",
     message: "You need to authenticate to access this content",
