b564ef03482667e3b5d9b9164e45ddc0b10b60f9
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-03-16 12:30:34 -0300

    feat(prisma): add prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.upsert({
+        where: {email: 'testing@testing.com'},
+        update: {},
+        create: {
+            email: 'testing@testing.com',
+            fullName: 'testing',
+            password: 'testing'
+        }
+    })
+}
+
+main()
+    .then(async()=> {
+        await prisma.$disconnect()
+    })
+    .catch(async(e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
