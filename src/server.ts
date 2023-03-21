f15bf291cedac6c97acb710fb9736ef343a8e502
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-03-21 20:21:28 -0300

    cd(deploy): update deploy.yml

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app, { init } from "@/app";
 
-const port = +process.env.PORT || 4000;
+const port = +process.env.PORT || 80;
 
 init().then(() => {
   app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server is listening on port ${port}.`);
   });
 });
