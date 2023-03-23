9a2cf9aec4d1ab844ba85b09ca9c88f2e38d1f51
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-03-23 13:46:46 -0300

    refacto(errros): add duplicated error object

--- src/services/user-services/errors.ts
+++ src/services/user-services/errors.ts
@@ -1,8 +1,10 @@
 import { ApplicationError } from "@/protocols";
 
+export const DuplicatedEmailErrorObject = {
+  name: "DuplicatedEmailError",
+  message: "There is already an user with given email",
+};
+
 export function duplicatedEmailError(): ApplicationError {
-  return {
-    name: "DuplicatedEmailError",
-    message: "There is already an user with given email",
-  };
+  return DuplicatedEmailErrorObject;
 }
